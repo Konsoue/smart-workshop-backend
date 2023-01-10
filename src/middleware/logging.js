2fa1750e2d6b703a7c2a58fa3e09ffa45545e798
konsoue <874111752@qq.com>
2023-01-10 11:18:39 +0800

    feat(log): 修改日志格式

--- src/middleware/logging.js
+++ src/middleware/logging.js
@@ -1,14 +1,16 @@
 const fs = require('fs')
 const path = require('path')
+const dayjs = require('dayjs')
 
 const LoggingRequestInfo = async (ctx, next) => {
   await next()
   const logContent = `-----------
+  请求时间: ${dayjs().format('YYYY-MM-DD hh:mm:ss')}
   请求的 IP 地址: ${ctx.request.ip}
   请求的 URL: ${ctx.request.url}
   `
   const logPath = path.join(process.cwd(), 'log.txt')
-  fs.writeFileSync(logPath, logContent)
+  fs.writeFileSync(logPath, logContent, { flag: 'a' })
 }
 
 module.exports = LoggingRequestInfo
